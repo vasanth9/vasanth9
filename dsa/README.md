f9e7ec2af76644fbcf0ceebe1ad6280660f27f64
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-05-02 13:56:42 +0530

    readme track script

--- dsa/README.md
+++ dsa/README.md
@@ -1,1 +1,8 @@
 # dsa
+
+|S.No| Problem | Website | Difficulty | Concept |
+| ----------- | ----------- | ----------- | ----------- | ----------- |
+| 1 | [find duplicates in an array](https://www.geeksforgeeks.org/problems/find-duplicates-in-an-array/1) | gfg | EASY | Array, Hashing|
+| 2 | [find duplicates in an array](https://www.geeksforgeeks.org/problems/find-duplicates-in-an-array/1) | gfg | EASY | Array, Hashing,|
+| 3 | [reverse a string](https://www.geeksforgeeks.org/problems/reverse-a-string/1) | gfg | EASY | String, Array|
+| 4 | [missing number in array1416](https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1) | gfg | EASY | Array, Sum of N natural numbers|
---
+++ dsa/track_files.py
@@ -0,0 +1,55 @@
+import os
+
+file_extensions = ['cpp','py']
+
+def extract_data(file):
+	f = open(file,"r")
+	link = f.readline().strip().split(" ")
+	type = f.readline().strip().split(" ")
+	concept  = f.readline().strip().split(" ")
+	data = []
+
+	if "link" in link:
+		data.append(link[3])
+	if "type" in type:
+		data.append(type[3])
+	if "concept" in concept:
+		data.append(" ".join(concept).split(":")[1])
+
+	return data
+
+def rip_the_hyphen(file):
+	extension = file.split(".")
+	if len(extension) > 1:
+		if extension[1] in file_extensions:
+			extracted_data = extract_data(file)
+			y = extension[0].split("-")
+			return [' '.join(y[:-1]),y[-1],extracted_data]
+			
+	
+	return ""
+		
+
+def write_to_md(data):
+	f = open("README.md","w")
+	f.write("# dsa\n\n")
+	f.write("|S.No| Problem | Website | Difficulty | Concept |\n")
+	f.write("| ----------- | ----------- | ----------- | ----------- | ----------- |\n")
+	index = 0
+	for d in data:
+		if len(d) > 2 :
+			if len(d[2])>0:
+				index+=1
+				f.write("| {} | [{}]({}) | {} | {} |{}|\n".format(index,d[0],d[2][0],d[1],d[2][1],d[2][2]))
+			
+		
+	f.close()
+
+# List all files in the current directory
+all_files = os.listdir()
+
+# List only files, excluding directories
+only_files = [f for f in os.listdir() if os.path.isfile(f)]
+file_data = map(rip_the_hyphen,only_files)
+write_to_md(file_data)
+#print(list(file_data))
