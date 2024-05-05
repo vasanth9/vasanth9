db3c981f701b694c58d7db685b71f9411ffc1f5b
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-05-05 11:50:43 +0530

    delete link list

---
+++ dsa/delete-node-in-a-linked-list-leetcode.cpp
@@ -0,0 +1,22 @@
+// link : https://leetcode.com/problems/delete-node-in-a-linked-list/
+// type : MEDIUM 
+// concept : Linked List
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
